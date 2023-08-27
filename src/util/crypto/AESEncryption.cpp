04af60e98aba627f58e5e5de175e24110ba11ba7
Thierry Cantin-Demers <piertcd@gmail.com>
2023-08-27 01:13:24 -0400

    Added AES encryption
    Uses openssl

---
+++ src/util/crypto/AESEncryption.cpp
@@ -0,0 +1,78 @@
+//
+// Created by fofi1 on 2023-08-27.
+//
+
+#include "AESEncryption.h"
+
+
+/**
+ * Encrypts the given message with AES using the given key.
+ * @param plainText the message to encrypt
+ * @param key the key used to encrypt the message
+ * @return the encrypted message
+ */
+std::string encryptAES(const std::string& plainText, const std::string& key)
+{
+    // Initialize the library
+    OpenSSL_add_all_algorithms();
+
+    // Create and initialize the context
+    EVP_CIPHER_CTX* ctx;
+    ctx = EVP_CIPHER_CTX_new();
+
+    // Initialize encryption operation
+    EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), nullptr, (const unsigned char*) key.c_str(), (const unsigned char*) "0123456789012345");
+
+    // Provide the plaintext and get the ciphertext
+    std::string encryptedText(plainText.length() + EVP_CIPHER_block_size(EVP_aes_128_cbc()), '\0');
+    int len;
+    EVP_EncryptUpdate(ctx, (unsigned char*) encryptedText.data(), &len, (const unsigned char*) plainText.c_str(), plainText.length());
+
+    // Finalize the encryption
+    int finalLen;
+    EVP_EncryptFinal_ex(ctx, (unsigned char*) encryptedText.data() + len, &finalLen);
+    len += finalLen;
+
+    // Clean up
+    EVP_CIPHER_CTX_free(ctx);
+    EVP_cleanup();
+
+    encryptedText.resize(len);
+    return encryptedText;
+}
+
+/**
+ * Decrypts the given message which was previously encoded with AES using the given key.
+ * @param encryptedText the encrypted message
+ * @param key the key used to encrypt the message
+ * @return the decrypted message
+ */
+std::string decryptAES(const std::string& encryptedText, const std::string& key)
+{
+    // Initialize the library
+    OpenSSL_add_all_algorithms();
+
+    // Create and initialize the context
+    EVP_CIPHER_CTX* ctx;
+    ctx = EVP_CIPHER_CTX_new();
+
+    // Initialize decryption operation
+    EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), nullptr, (const unsigned char*) key.c_str(), (const unsigned char*) "0123456789012345");
+
+    // Provide the ciphertext and get the plaintext
+    std::string decryptedText(encryptedText.length(), '\0');
+    int len;
+    EVP_DecryptUpdate(ctx, (unsigned char*) decryptedText.data(), &len, (const unsigned char*) encryptedText.c_str(), encryptedText.length());
+
+    // Finalize the decryption
+    int finalLen;
+    EVP_DecryptFinal_ex(ctx, (unsigned char*) decryptedText.data() + len, &finalLen);
+    len += finalLen;
+
+    // Clean up
+    EVP_CIPHER_CTX_free(ctx);
+    EVP_cleanup();
+
+    decryptedText.resize(len);
+    return decryptedText;
+}
