94390885e5e9fdfc831c65da9350770c718c7f6a
Thierry Cantin-Demers <piertcd@gmail.com>
2023-08-30 21:14:20 -0400

    Put function declarations in blocks

--- src/Model/ApplicationLogic/UserLogic.h
+++ src/Model/ApplicationLogic/UserLogic.h
@@ -11,21 +11,24 @@ class UserLogic
 public:
     UserLogic(User& user, IChatAPI& chatAPI) : user(user), chatAPI(chatAPI){};
 
-    void AppendSelectedChatHistory(const std::string& message);
-    void AddChatMessageToPeerChatHistory(const std::string& peerId, const ChatMessage& chatMessage);
-    void CreateNewChatHistory(const std::string& peerId);
-    void UpdatePeerState(const std::string& peerId, const ConnectionState& state);
-    void IncrementPeerUnreadMessageCount(const std::string& peerId);
-    void Reset(const std::string& username);
-    [[nodiscard]] const std::string& GetUserName() const;
-    [[nodiscard]] const size_t& GetMaxNameLength() const;
     void LoginWithNewUser(const std::string& username_);
-    bool IsClientConnected() const;
-    [[nodiscard]] const std::unordered_map<std::string, PeerData>& GetPeerDataMap() const;
+    void Reset(const std::string& username);
+    [[nodiscard]] bool IsClientConnected() const;
+
     const void SendTextMessage(const std::string& message);
     void AddNewChatPeer(const std::string& peerId);
     void SetSelectedPeerId(const std::string& peerId);
+    void UpdatePeerState(const std::string& peerId, const ConnectionState& state);
+    void IncrementPeerUnreadMessageCount(const std::string& peerId);
+
+    void CreateNewChatHistory(const std::string& peerId);
+    void AppendSelectedChatHistory(const std::string& message);
+    void AddChatMessageToPeerChatHistory(const std::string& peerId, const ChatMessage& chatMessage);
+    
+    [[nodiscard]] const std::unordered_map<std::string, PeerData>& GetPeerDataMap() const;
     [[nodiscard]] const ChatHistory* GetSelectedChatHistory() const;
+    [[nodiscard]] const std::string& GetUserName() const;
+    [[nodiscard]] const size_t& GetMaxNameLength() const;
 
 private:
     User& user;
